eba380bf2faf90372cbd9d03247218f0a77fb93d
GOXR3PLUS <crAlexcomp@gmail.com>
2017-05-23 20:11:52 +0300

    Update

--- URLReader.java
+++ URLReader.java
@@ -1,12 +1,8 @@
-package aaTester;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
-
-import org.apache.commons.io.IOUtils;
 
 public class URLReader {
 	public static void main(String[] args) throws Exception {
