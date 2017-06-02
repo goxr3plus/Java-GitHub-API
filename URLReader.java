1f9674b57c3edfc625410ab157e25a577e94d906
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2017-06-02 05:04:05 +0300

    Update URLReader.java

--- URLReader.java
+++ URLReader.java
@@ -7,27 +7,26 @@ import java.util.Arrays;
 public class URLReader {
 	public static void main(String[] args) throws Exception {
 		
-
 		//Create HttpURLConnection 
 		HttpURLConnection httpcon = (HttpURLConnection) new URL("https://api.github.com/repos/goxr3plus/XR3Player/releases").openConnection();
 		httpcon.addRequestProperty("User-Agent", "Mozilla/5.0");
 		BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream()));
 		
 		//Read line by line
-		String line = "" , inputLine;
-		while ( ( inputLine = in.readLine() ) != null) {
-			line += "\n" + inputLine;
-			System.out.println(inputLine);
+		StringBuilder responseSB = new StringBuilder();
+		String line;
+		while ( ( line = in.readLine() ) != null) {
+			responseSB.append("\n" + line);
+			//System.out.println(line);
 		}
 		in.close();
 		
 		//Get Git Hub Downloads of XR3Player
-	     Arrays.stream(line.split("\"download_count\":")).skip(1).map(l -> l.split(",")[0]).forEach(l -> System.out.println(l));
-				
-
-	    //Sum up all download counts
-	    int total = Arrays.stream(line.split("\"download_count\":")).skip(1).mapToInt(l -> Integer.parseInt(l.split(",")[0])).sum();		
-	    System.out.println("\nTotal Downloads: " + total);
+		Arrays.stream(responseSB.toString().split("\"download_count\":")).skip(1).map(l -> l.split(",")[0]).forEach(l -> System.out.println(l));
+		
+		//Sum up all download counts
+		int total = Arrays.stream(responseSB.toString().split("\"download_count\":")).skip(1).mapToInt(l -> Integer.parseInt(l.split(",")[0])).sum();
+		System.out.println("\nTotal Downloads: " + total);
 		
 	}
 	
